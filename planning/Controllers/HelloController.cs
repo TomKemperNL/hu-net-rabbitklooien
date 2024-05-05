f608809f6421bc8f79197f9b8d929961c0e7729d
Tom <tom.kemper@hu.nl>
2024-05-05 10:55:24 +0200

    ok, dus zo werkt webapi routing...

---
+++ planning/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Planning.Inno.HU.Controllers;
+
+[ApiController]
+[Route("/hello")]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<Message> Test()
+    {
+        return new Message("Hello World");
+    }
+}
+
+public class Message
+{
+    public Message(string value)
+    {
+        Value = value;
+    }
+
+    public String Value { get; private set; }
+}
