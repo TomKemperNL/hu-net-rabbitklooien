a8a744442ae0edd32f2e7fcff4c39e1775e631a8
Tom <tom.kemper@hu.nl>
2024-05-13 09:32:10 +0200

    Scoping & Dependency-Injection voor de listeners

--- teachers/Messaging/RabbitConsumer.cs
+++ teachers/Messaging/RabbitConsumer.cs
@@ -4,33 +4,47 @@ using RabbitMQ.Client.Events;
 
 namespace HU.Inno.Teachers.Messaging;
 
-public class RabbitConsumer<T>
+public class RabbitConsumer<T> : IDisposable
 {
     private readonly IConnection _rabbitConnection;
+    private readonly IServiceProvider _appServices;
     private readonly string _queue;
-    private readonly Action<T> _consumer;
+    private readonly Action<IServiceProvider, T> _consumer;
 
-    public RabbitConsumer(IConnection rabbitConnection, string queue, Action<T> consumer)
+    public RabbitConsumer(IConnection rabbitConnection, IServiceProvider appServices, string queue, Action<IServiceProvider, T> consumer)
     {
         _rabbitConnection = rabbitConnection;
+        _appServices = appServices;
         _queue = queue;
         _consumer = consumer;
     }
 
+    private IModel? _channel;
+    private EventingBasicConsumer? _eventingBasicConsumer;
+    
     public void Start()
     {
-        IModel channel = _rabbitConnection.CreateModel();
-
-        var consumer = new EventingBasicConsumer(channel);
-        consumer.Received += (ch, ea) =>
+        _channel = _rabbitConnection.CreateModel();
+        _eventingBasicConsumer = new EventingBasicConsumer(_channel);
+        _eventingBasicConsumer.Received += (ch, ea) =>
         {
             Console.WriteLine("Receiving");
             var body = ea.Body.ToArray();
             T message = JsonSerializer.Deserialize<T>(body)!;
-            _consumer(message);
+
+            using (var scope = _appServices.CreateScope())
+            {
+                _consumer(scope.ServiceProvider, message);    
+            }
         };
         Console.WriteLine("Consuming");
-        channel.BasicConsume(_queue, true, consumer);
+        _channel.BasicConsume(_queue, true, _eventingBasicConsumer);
         Console.WriteLine("???");
+    }
+
+    public void Dispose()
+    {
+        _rabbitConnection.Dispose();
+        _channel.Dispose();
     }
 }
--- teachers/Messaging/RabbitStartup.cs
+++ teachers/Messaging/RabbitStartup.cs
@@ -24,15 +24,22 @@ public class RabbitStartup
         }
     }
 
+
+    private List<IDisposable> _consumers = new List<IDisposable>();
+    
     public void ConfigureSubscribers(IServiceProvider appServices)
     {
         IConnection mainConnection = appServices.GetService<IConnection>();
+        
         RabbitConsumer<TeacherCreated> createdListener = new RabbitConsumer<TeacherCreated>(mainConnection,
+            appServices,
             TeacherCreatedQueue,
-            tc =>
+            (services, tc) =>
             {
                 Console.WriteLine("Created: " + tc.Email);
             });
+        
+        _consumers.Add(createdListener);
         Console.WriteLine("Starting MessageProcessors");
         createdListener.Start();
     }
@@ -40,8 +47,17 @@ public class RabbitStartup
     public void ConfigurePublishers(IServiceCollection builderServices)
     {
         EnsureConnected();
+        // https://www.rabbitmq.com/client-libraries/dotnet-api-guide#connection-and-channel-lifespan
+        // Een connection mag over de applicatie geshared worden
         builderServices.AddSingleton(_connection);
+        
+        // Maar een channel mag niet door meerdere publishers of consumers tegelijk gebruikt worden.
+        // AddScoped zorgt voor 1 dedicated channel per request. En een request wordt gewoonlijk serieel afgehandeld.
         builderServices.AddScoped<IModel>(s => { return s.GetService<IConnection>()!.CreateModel(); });
+        
+        // De Publisher zelf is nu transient (dus twee losse services in hetzelfde request krijgen eigen instances), 
+        // dat is misschien niet optimaal: AddScoped kan hier vast ook. Maar ik default naar 'zo min mogelijk delen',
+        // en deze kleine classjes zijn geen dure resources (in tegenstelling tot de channel of connection).
         builderServices.AddTransient<IPublisher<TeacherCreated>>(s =>
             new RabbitPublisher<TeacherCreated>(
                 TeacherCreatedQueue, s.GetService<IModel>()!
